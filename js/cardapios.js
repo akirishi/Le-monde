3aa6602575ca31d71fc7b86b41841eae3b570e0e
akirishi <90563712+akirishi@users.noreply.github.com>
2022-03-30 16:14:46 -0300

    Add files via upload

--- js/cardapios.js
+++ js/cardapios.js
@@ -2,27 +2,39 @@ $(document).ready(function () {
   $('#padrao').click(function () {
     if ($('#tematico').hasClass('visible')) {
       $("#tematico").css("display", "none");
+      $('#info').css('display', 'none')
       $('#tematico').addClass('hidden');
+      $('#info').addClass('hidden')
       $('#tematico').removeClass('visible')
-      $('#padrao figcaption').css('display', 'none')
+      $('#info').removeClass('visible')
+      $('figure#padrao').css({'width': '200%'})
     } else {
       $('#tematico').css('display', 'flex');
+      $('#info').css('display', 'block')
       $('#tematico').addClass('visible');
+      $('#info').addClass('visible')
       $('#tematico').removeClass('hidden')
-      $('#padrao figcaption').css('display', 'flex')
+      $('#info').removeClass('hidden')
+      $('figure#padrao').css({'display': 'flex', 'width': '100%'})
     };
   });
   $('#tematico').click(function () {
     if ($('#padrao').hasClass('visible')) {
       $("#padrao").css("display", "none");
+      $('#info').css('display', 'none')
       $('#padrao').addClass('hidden');
+      $('#info').addClass('hidden')
       $('#padrao').removeClass('visible')
-      $('#tematico figcaption').css('display', 'none')
+      $('#info').removeClass('visible')
+      $('figure#tematico').css('width', '200%')
     } else {
       $('#padrao').css('display', 'flex');
+      $('#info').css('display', 'block')
       $('#padrao').addClass('visible');
+      $('#info').addClass('visible')
       $('#padrao').removeClass('hidden')
-      $('#tematico figcaption').css('display', 'flex')
+      $('#info').removeClass('hidden')
+      $('figure#tematico').css({'display': 'flex', 'width': '100%'})
     };
   });
 });
